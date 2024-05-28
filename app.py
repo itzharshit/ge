aea3005a2687b842b47a0f01ad6216f126b298a8
Harshit Shrivastav <87630299+itzharshit@users.noreply.github.com>
2024-05-28 16:42:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -31,4 +31,4 @@ def homepage():
     return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80, debug=True)
+    app.run(host='0.0.0.0', port=8090, debug=True)
